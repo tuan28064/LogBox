3128bc07cbe685eb5fa2eae2e4b50d60699e5c7e
wangjingtuan <wangjingtuan@qiyi.com>
2017-07-24 20:51:37 +0800

    Improvement: 增加close和clear功能
    Signed-off-by: wangjingtuan <wangjingtuan@qiyi.com>

--- LogBox/LogBox.m
+++ LogBox/LogBox.m
@@ -13,6 +13,8 @@
 #define LBWindowPadding 20
 #define LBTabBarHeight 64
 #define LB_STRING_IS_NOT_NULL(string) ([string isKindOfClass:[NSString class]] && [string length] && string != nil)
+#define LBButtonWidth 60
+#define LBButtonHeight 28
 
 #pragma mark - <<<<<<<<<<<<<<<<<<<< LBViewController >>>>>>>>>>>>>>>>>>>>
 
@@ -20,7 +22,9 @@
 
 @property (nonatomic, strong) UITextView      *lbTextView;
 @property (nonatomic, strong) NSMutableArray  *lbLogArray;
-@property (nonatomic, strong) NSDateFormatter *dateFormatter;
+@property (nonatomic, strong) NSDateFormatter *lbDateFormatter;
+@property (nonatomic, strong) UIButton        *lbCloseButton;
+@property (nonatomic, strong) UIButton        *lbClearButton;
 
 @end
 
@@ -48,6 +52,7 @@
 
         // log不为空，初始化
         [self __initTextView];
+        [self __initButton];
         [self __initLogArray];
         [self __initDateFormatter];
 
@@ -62,7 +67,7 @@
 {
     NSMutableString *logString = [NSMutableString string];
     for (NSString *string in _lbLogArray) {
-        [logString appendFormat:@"%@ \n %@ \n", [_dateFormatter stringFromDate:[NSDate date]], string];
+        [logString appendFormat:@"%@ \n %@ \n", [_lbDateFormatter stringFromDate:[NSDate date]], string];
     }
 
     _lbTextView.text = logString;
@@ -76,7 +81,7 @@
     if (!_lbTextView) {
 
         CGRect textViewFrame = [UIScreen mainScreen].bounds;
-        _lbTextView = [[UITextView alloc]initWithFrame:CGRectMake(0, 0, textViewFrame.size.width - LBWindowPadding * 2, textViewFrame.size.height - LBTabBarHeight - LBWindowPadding * 2)];
+        _lbTextView = [[UITextView alloc]initWithFrame:CGRectMake(0, 4 * 2 + LBButtonHeight, textViewFrame.size.width - LBWindowPadding * 2, textViewFrame.size.height - LBTabBarHeight - LBWindowPadding * 2 - 4 * 2 - LBButtonHeight)];
         _lbTextView.backgroundColor = [UIColor clearColor];
         _lbTextView.delegate = self;
         _lbTextView.font = [UIFont systemFontOfSize:14.0];
@@ -85,8 +90,38 @@
         _lbTextView.editable = NO;
         _lbTextView.selectable = NO;
 
-        [self.view addSubview:_lbTextView];
     }
+
+    [self.view addSubview:_lbTextView];
+}
+
+- (void)__initButton
+{
+    if (!_lbCloseButton) {
+
+        _lbCloseButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        _lbCloseButton.frame = CGRectMake([UIScreen mainScreen].bounds.size.width - 2 * LBWindowPadding - LBButtonWidth - 4, 4, LBButtonWidth, LBButtonHeight);
+        _lbCloseButton.backgroundColor = LBThemeColor;
+        [_lbCloseButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [_lbCloseButton setTitle:@"Close" forState:UIControlStateNormal];
+        [_lbCloseButton addTarget:self action:@selector(__closeButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+
+    }
+
+    [self.view addSubview:_lbCloseButton];
+
+    if (!_lbClearButton) {
+
+        _lbClearButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        _lbClearButton.frame = CGRectMake([UIScreen mainScreen].bounds.size.width - 2 * LBWindowPadding - 2 * (4 + LBButtonWidth), 4, LBButtonWidth, LBButtonHeight);
+        _lbClearButton.backgroundColor = LBThemeColor;
+        [_lbClearButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [_lbClearButton setTitle:@"Clear" forState:UIControlStateNormal];
+        [_lbClearButton addTarget:self action:@selector(__clearButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+
+    }
+
+    [self.view addSubview:_lbClearButton];
 }
 
 - (void)__initLogArray
@@ -98,10 +133,24 @@
 
 - (void)__initDateFormatter
 {
-    if (!_dateFormatter) {
-        _dateFormatter = [[NSDateFormatter alloc]init];
-        [_dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss.sss"];
+    if (!_lbDateFormatter) {
+        _lbDateFormatter = [[NSDateFormatter alloc]init];
+        [_lbDateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss.sss"];
     }
+}
+
+#pragma mark -------------------- action methods --------------------
+
+- (void)__closeButtonClick:(UIButton *)button
+{
+    self.view.window.hidden = YES;
+}
+
+- (void)__clearButtonClick:(UIButton *)button
+{
+    [_lbLogArray removeAllObjects];
+
+    _lbTextView.text = @"";
 }
 
 @end
@@ -137,37 +186,14 @@
     }
 
     // 初始化
-    [self __enableLogBox];
-
-    [_lbViewController showLogWithContent:content];
-}
-
-- (void)__enableLogBox
-{
     [self __initLogboxWindow];
 
-    // 添加开启手势
-    UISwipeGestureRecognizer *swipeOpenGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(__onSwipeOpenDetected:)];
-    swipeOpenGesture.numberOfTouchesRequired = 3;
-    swipeOpenGesture.direction = UISwipeGestureRecognizerDirectionLeft;
-    [[UIApplication sharedApplication].keyWindow addGestureRecognizer:swipeOpenGesture];
-
-    // 添加关闭手势
-    UISwipeGestureRecognizer *swipeCloseGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(__onSwipeCloseDetected:)];
-    swipeCloseGesture.numberOfTouchesRequired = 3;
-    swipeCloseGesture.direction = UISwipeGestureRecognizerDirectionRight;
-    [_logBoxWindow addGestureRecognizer:swipeCloseGesture];
-    
+    [_lbViewController showLogWithContent:content];
 }
 
 - (void)__onSwipeOpenDetected:(UISwipeGestureRecognizer *)gesture
 {
     _logBoxWindow.hidden = NO;
-}
-
-- (void)__onSwipeCloseDetected:(UISwipeGestureRecognizer *)gesture
-{
-    _logBoxWindow.hidden = YES;
 }
 
 - (void)__initLogboxWindow
@@ -189,6 +215,12 @@
 
     }
     _logBoxWindow.hidden = NO;
+
+    // 添加开启手势
+    UISwipeGestureRecognizer *swipeOpenGesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(__onSwipeOpenDetected:)];
+    swipeOpenGesture.numberOfTouchesRequired = 5;
+    swipeOpenGesture.direction = UISwipeGestureRecognizerDirectionLeft;
+    [[UIApplication sharedApplication].keyWindow addGestureRecognizer:swipeOpenGesture];
 }
 
 @end
